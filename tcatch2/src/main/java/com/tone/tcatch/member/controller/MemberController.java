62d984de0e6d56606bfc5a3b7055b3ec7a00a02d
iheee6-6 <iheee6@gmail.com>
2020-02-05 21:01:24 +0900

    init

---
+++ tcatch2/src/main/java/com/tone/tcatch/member/controller/MemberController.java
@@ -0,0 +1,18 @@
+package com.tone.tcatch.member.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MemberController {
+	
+	@RequestMapping("/login.do")
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping("enrollView.do")
+	public String enrollView() {
+		return "member/memberJoin";
+	}
+}
