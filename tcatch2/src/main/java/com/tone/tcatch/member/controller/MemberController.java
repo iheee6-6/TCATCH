7148df5d12cc36b1f8d1839bbd4d62c1fc822682
iheee6-6 <58504019+iheee6-6@users.noreply.github.com>
2020-02-14 20:09:49 +0900

    Update MemberController.java

--- tcatch2/src/main/java/com/tone/tcatch/member/controller/MemberController.java
+++ tcatch2/src/main/java/com/tone/tcatch/member/controller/MemberController.java
@@ -16,33 +16,5 @@ public class MemberController {
 		return "member/memberJoin";
 	}
 	
-	@RequestMapping("/enterMypage.do")
-	public String enterMypage() {
-		return "mypage/firstPage";
-	}
 	
-	@RequestMapping("/interestPerformance.do")
-	public String selectInterestP() {
-		return "mypage/interestPerformance";
-	}
-	
-	@RequestMapping("/alarmList.do")
-	public String AlarmList() {
-		return "mypage/alarmList";
-	}
-	
-	@RequestMapping("/checknCancel.do")
-	public String ticketChecknCancel() {
-		return "mypage/ChecknCancel";
-	}
-	
-	@RequestMapping("/cncDetail.do")
-	public String cncDetail() {
-		return "mypage/CnCDetail";
-	}
-	
-	@RequestMapping("/viewPerformance.do")
-	public String viewPerformance() {
-		return "mypage/viewPerformance";
-	}
 }
