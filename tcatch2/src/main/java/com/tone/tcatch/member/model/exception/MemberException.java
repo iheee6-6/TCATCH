3267988df5fd9d4af29d9b90b3c8860963b820ad
user2 <user2@user2-PC>
2020-02-18 16:06:39 +0900

    0218	1602

--- tcatch2/src/main/java/com/tone/tcatch/member/model/exception/MemberException.java
+++ tcatch2/src/main/java/com/tone/tcatch/member/model/exception/MemberException.java
@@ -1,5 +1,7 @@
 package com.tone.tcatch.member.model.exception;
 
-public class MemberException {
-
+public class MemberException extends RuntimeException{
+	public MemberException(String msg) {
+		super(msg);
+	}
 }
